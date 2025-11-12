5f07ecea3f34604e53524dd6eefe1311843a3b88
caroldmg <carolinadmg98@gmail.com>
2025-11-12 20:24:38 +0100

    creados archivs de cpp05 ex02

---
+++ cpp_05/ex02/inc/RobotomyRequestForm.hpp
@@ -0,0 +1,5 @@
+/* 
+	• RobotomyRequestForm: Required grades: sign 72, exec 45
+Makes some drilling noises, then informs that <target> has been robotomized
+successfully 50% of the time. Otherwise, it informs that the robotomy failed.
+*/
---
+++ cpp_05/ex02/inc/ShrubberyCreationForm.hpp
@@ -0,0 +1,5 @@
+/* 
+	ShrubberyCreationForm: Required grades: sign 145, exec 137
+Creates a file <target>_shrubbery in the working directory and writes ASCII trees
+inside it.
+*/
