9665f67fefc8d116fb0ed02674a3952455372420
caroldmg <carolinadmg98@gmail.com>
2026-01-11 18:01:06 +0100

    cpp07 ex00 modificado tyename en lugar de class en .hpp y cambiado el nombre del ejecutable

--- cpp_07/ex00/whatever.hpp
+++ cpp_07/ex00/whatever.hpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
 
-template<class T> void swap(T& a, T& b)
+template<typename T> void swap(T& a, T& b)
 {
 	T c(a);
 	a = b;
 	b = c;
 }
 
-template<class T> T min(T& a, T& b)
+template<typename T> T min(T& a, T& b)
 {
 	if (a > b)
 		return (b);
 	return (a);
 }
 
-template<class T> T max(T& a, T& b)
+template<typename T> T max(T& a, T& b)
 {
 	if (a > b)
 		return (a);
